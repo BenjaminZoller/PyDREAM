d6842823d5ccd630b3e808103d7c7d750d70b1bf
Benjamin Zoller <benjamin.zoller@servier.com>
2026-01-31 00:55:54 +0100

    flatnonzero is cleaner with explicit int

--- pydream/Dream.py
+++ pydream/Dream.py
@@ -465,7 +465,7 @@ class Dream():
         cross_probs = Dream_shared_vars.cross_probs[0:self.nCR]
         
         #Compute squared normalized jumping distance
-        m_loc = np.where(self.CR_values == CR)[0].item()
+        m_loc = int(np.flatnonzero(self.CR_values == CR)[0])
 
         Dream_shared_vars.ncr_updates[m_loc] += 1
         
